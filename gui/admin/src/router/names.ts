8f5ef659e302963d5876b7c85f0e2808c99654f9
dcq <dariancapoteq@gmail.com>
2022-08-29 23:45:35 -0400

    Add route for Auth Page.

---
+++ gui/admin/src/router/names.ts
@@ -0,0 +1,4 @@
+export enum ROUTE_NAME {
+  LOGIN = 'login',
+  HOME = 'home',
+}
--- gui/admin/src/router/routes.ts
+++ gui/admin/src/router/routes.ts
@@ -1,10 +1,18 @@
 import { RouteRecordRaw } from 'vue-router';
+import { ROUTE_NAME } from './names';
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  },
+
+  {
+    path: '/login',
+    name: ROUTE_NAME.LOGIN,
+    component: () => import('layouts/AuthLayout.vue'),
+    children: [{ path: '', component: () => import('pages/LoginPage.vue') }],
   },
 
   // Always leave this as last one,
