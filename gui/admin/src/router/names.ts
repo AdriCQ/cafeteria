ad1239ac67717877e7857e7d6292f390c8a34fab
dcq <dariancapoteq@gmail.com>
2022-08-31 17:39:25 -0400

    Add more links.

--- gui/admin/src/router/names.ts
+++ gui/admin/src/router/names.ts
@@ -1,4 +1,10 @@
 export enum ROUTE_NAME {
   LOGIN = 'login',
   HOME = 'home',
+  PROFILE = 'profile',
+  APP_CONFIG = 'app-configuration',
+  OFFERS = 'offers',
+  EVENTS = 'events',
+  NEWS = 'news',
+  COMMENTS = 'comments',
 }
