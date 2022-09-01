85bc395157c71a150d0c68708074099576d7f84e
AdriCQ <adrian.capote95@zoho.com>
2022-08-31 22:30:31 -0400

    Add Capacitor Android

---
+++ gui/ui/android/app/src/main/java/com/servimav/jaguabit/MainActivity.java
@@ -0,0 +1,5 @@
+package com.servimav.jaguabit;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ gui/ui/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.servimav.jaguabit',
+  appName: 'Jagua-BIT',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
